cbd6dcdc427a259822ca9d429451a126f53fdca4
gui-rossi <guizo.rossi@gmail.com>
2021-04-10 14:54:39 -0300

    fix build errors

--- android/app/src/main/java/com/rn/MainActivity.java
+++ android/app/src/main/java/com/rn/MainActivity.java
@@ -1,5 +1,10 @@
 package com.rn;
 
+import java.util.List;
+import java.util.Arrays;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.react.ReactPackage;
+
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -8,16 +13,13 @@ public class MainActivity extends ReactActivity {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  @Override
   protected String getMainComponentName() {
     return "rn";
   }
 
-  @Override
   protected List<ReactPackage> getPackages() {
       return Arrays.asList(
-              new MainReactPackage(),
-              new ReactMaterialKitPackage()
+              new MainReactPackage()
       );
   }
 
